ca49ac737f76656c7903a97c02386ba71a4ceb13
ツジ <ituzi27@gmail.com>
2025-08-01 22:35:11 +0900

    原音喋る接路音が出現

---
+++ script/log_dot_sound.js
@@ -0,0 +1,66 @@
+let dotImage = null; //document.getElementById("log_dot");
+let logSound = document.getElementsByClassName("sound");
+let loghanasu = null; //document.getElementById("log_sound_hanasu");
+
+const beforeImage = `<img id="log_dot_imag" src="./imag/log_dot/log_d0.PNG" onmouseover="this.src='./imag/log_dot/log_d1.PNG'" onmouseout="this.src='./imag/log_dot/log_d0.PNG'">`;
+const mabatakiImage = `<img id="log_dot_imag" src="./imag/log_dot/log_d2.PNG" onmouseover="this.src='./imag/log_dot/log_d1.PNG'" onmouseout="this.src='./imag/log_dot/log_d2.PNG'">`;
+
+const logImage = ["log_d3.PNG", "log_d4.PNG", "log_d5.PNG"];
+const SampleSound = ["sample_k0.wav", "sample_k1.wav", "sample_m0.wav", "sample_m1.wav", "sample_m2.wav"];
+
+let soundNum;
+let isLooping = false;
+
+function RandInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function log_update(){
+    dotImage = document.getElementById("log_dot");
+    loghanasu = document.getElementById("log_sound_hanasu");
+}
+
+function choiceSound(){
+    dotImage.innerHTML = `<img id="log_dot_imag" src="imag/log_dot/${logImage[RandInt(3)]}">`;
+    soundNum = RandInt(5);
+    const audioSrc = `sample/log_samp/${SampleSound[soundNum]}`;
+
+    const audio = document.createElement('audio');
+    audio.src = audioSrc;
+    audio.className = "sound";
+    audio.autoplay = true;
+    hanasu();
+
+    audio.onended = (event) => {
+        dotImage.innerHTML = beforeImage;
+        loghanasu.innerHTML = "";
+    };
+}
+
+function hanasu(){
+    if(soundNum <= 2){
+        loghanasu.innerHTML = `<br><br><br>
+                        <div id="log_hanasu">キーボード.wav</div>`;
+    }else{
+        loghanasu.innerHTML = `<br><br><br>
+                        <div id="log_hanasu">マウス.wav</div>`;
+    }
+}
+
+function log_Onsound(){
+    choiceSound();
+}
+
+function mabataki(){
+    dotImage.innerHTML = mabatakiImage;
+    setTimeout(() => {
+        dotImage.innerHTML = beforeImage;
+    }, 200);
+}
+
+function loopMabataki(){
+        mabataki();
+        const next = RandInt(7)*1000;
+        setTimeout(loopMabataki, next);
+}
+loopMabataki();
