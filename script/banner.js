56f02c06f8e7b4d2efc6b661128a1a9f39614388
ツジ <ituzi27@gmail.com>
2026-03-19 07:37:23 +0900

    banner.js を更新

--- script/banner.js
+++ script/banner.js
@@ -5,8 +5,8 @@ for (let i = 0; i < banners.length; i++) {
         if (confirm("バナーを保存しますか？🐟")) {
             const link = document.createElement("a");
             link.href = this.src;
-            if (i == 0) link.download = 'd20040.gif';
-            if (i == 1) link.download = 'd8831.gif';
+            if (i == 1) link.download = 'd20040.gif';
+            if (i == 2) link.download = 'd8831.gif';
             link.click();
         }
     });
