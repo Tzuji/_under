f1082465a93c61e70658135ced5d80aee97b2186
ツジ <ituzi27@gmail.com>
2026-04-14 05:15:53 +0900

    Delete script/error.js

--- script/error.js
+++
@@ -1,9 +0,0 @@
-let cont = 1.5;
-let skew = 3;
-
-function triggerCrash() {
-
-    document.body.style.filter = "contrast(" + cont + ")";
-    document.body.style.transform = "skewX(" + skew + "deg)";
-    cont+=1.0;
-}
